76e3732eb4206a5249a2d81c3cc0fa0f7bf40ba9
Jeff-021 <jefersonrg2017@gmail.com>
2024-09-25 20:08:00 -0300

    1

--- exer03-exercicio/rpg.js
+++ exer03-exercicio/rpg.js
@@ -1,25 +1,33 @@
-const nameCharacter1 = prompt("Digite o nome de um personagem");
-const attackPower1 = prompt(
-  "Digite também a quantidade de poder de ataque que ele possui"
-);
-const nameCharacter2 = prompt(
-  "Digite o nome de um outro persoangem, mas esse será um personagem de defesa"
-);
-const defensePower1 = prompt("Digite um poder de defesa que ele irá possuir");
-const shield1 = prompt("Quantos de escudo ele terá a mais na sua vida?");
-confirm("Confirma as informações?");
+document.getElementById("button").addEventListener("click", function () {
+  const name1 = document.getElementById("nome1").value;
+  const attack = parseFloat(document.getElementById("attack").value);
+  const name2 = document.getElementById("nome2").value;
+  const defense = parseFloat(document.getElementById("defense").value);
+  const shild = parseFloat(document.getElementById("shild").value);
 
-const attackPower = parseFloat(attackPower1);
-const defensePower = parseFloat(defensePower1);
-const shield = parseFloat(shield1);
-const statics = defensePower + shield - attackPower;
+  const surviverP = document.createElement("p");
+  const deathP = document.createElement("p");
+  const empateP = document.createElement("p");
+  const liveShild = defense + shild;
 
-if (attackPower >= defensePower + shield) {
-  alert(
-    `O personagem ${nameCharacter2} infelizmente não resistiu ao ataque, mesmo com a ajuda do escudo, ele ficou com: ${statics} pontos de vida`
-  );
-} else {
-  alert(
-    `O personagem ${nameCharacter2} sobreviveu, pois com a ajuda do escudo ele é mais forte!, ele ficou com ${statics} pontos de vida `
-  );
-}
+  if (attack > defense + shild) {
+    deathP.textContent = `O personagem ${name1}, com um poder de ataque de ${attack} pontos, acabou matando o personagem ${name2}. A defesa somada ao escudo totalizou ${liveShild} pontos de vida, não foi suficiente para protegê-lo!`;
+  } else if (attack === defense + shild) {
+    empateP.textContent = `O personagem ${name2} não resistiu. Mesmo com a ajuda do escudo, ele apenas igualou o ataque de ${name1}, que era de ${attack} pontos. Os pontos de vida de ${name2}, somados com o escudo, totalizaram ${attack} pontos, totalizando a sua morte!.`;
+  } else if (defense + shild > attack) {
+    surviverP.textContent = `O personagem ${name2} resistiu ao ataque de ${name1}, graças à defesa e ao escudo que foram superiores ao ataque dê: ${attack} pontos, a vida com o escudo do personagem ${name2} totalizados foram dê: ${liveShild} pontos! .`;
+  }
+
+  const resultadoDiv = document.getElementById("resultado");
+  resultadoDiv.innerHTML = "";
+
+  resultadoDiv.appendChild(surviverP);
+  resultadoDiv.appendChild(deathP);
+  resultadoDiv.appendChild(empateP);
+
+  document.getElementById("nome1").value = "";
+  document.getElementById("attack").value = "";
+  document.getElementById("nome2").value = "";
+  document.getElementById("defense").value = "";
+  document.getElementById("shild").value = "";
+});
--- exer03-exercicio/velocidade.js
+++ exer03-exercicio/velocidade.js
@@ -1,23 +1,33 @@
-const carOne = prompt("Digite o nome do primeiro carro");
-const speedOne = prompt("Digite a velocidade dele");
-const carTwo = prompt("Digite o nome do segundo carro");
-const speedTwo = prompt("Digite a velocidade dele");
+document.getElementById("button").addEventListener("click", function () {
+  const name1 = document.getElementById("nome").value;
+  const speed1 = parseFloat(document.getElementById("velocidade1").value);
+  const name2 = document.getElementById("nome2").value;
+  const speed2 = parseFloat(document.getElementById("velocidade2").value);
 
-const speed1 = parseFloat(speedOne);
-const speed2 = parseFloat(speedTwo);
+  const nameP = document.createElement("p");
+  const name2P = document.createElement("p");
+  const name3P = document.createElement("p");
 
-if (speed1 > speed2) {
-  alert(
-    `O carro ${carOne} é mais rapido, pois sua velocidade é ${speed1} km/h`
-  );
-} else if (speed1 === speed2) {
-  alert(
-    `As velocidades do ${carOne} e ${carTwo} são as mesmas, cada um tem a velocidade dê: ${speed1} km/h`
-  );
-} else {
-  alert(
-    `O carro ${carTwo} é mais rapido, pois a velocidade dele é ${speed2} km/h`
-  );
-}
+  const compararSpeed1 = speed1 - speed2;
+  const compararspeed2 = speed2 - speed1;
 
-// o if serve apenas para true, se for truel vai fazer uma acao, se for false ele vai fazer outra coma ajuda do else
+  if (speed1 > speed2) {
+    nameP.textContent = `O carro ${name1} é mais rápido, pois a velocidade dele é dê ${speed1}, que é ${compararSpeed1} km/h mais rápido! que o ${name2}, com a velçocidade de ${speed2} km/h`;
+  } else if (speed1 === speed2) {
+    name2P.textContent = `Os carros: ${name1} e ${name2}, tem a mesma velocidade que é dê: ${speed1}`;
+  } else if (speed1 < speed2) {
+    name3P.textContent = `O carro ${name2} é mais rápido, pois ele possui a velocidade dê: ${speed2}, que é ${compararspeed2} km/h mais rápido! que o ${name1}, com a velçocidade de ${speed1}`;
+  }
+
+  const resultadoDiv = document.getElementById("resultado");
+  resultadoDiv.innerHTML = "";
+
+  resultadoDiv.appendChild(nameP);
+  resultadoDiv.appendChild(name2P);
+  resultadoDiv.appendChild(name3P);
+
+  document.getElementById("nome").value = "";
+  document.getElementById("velocidade1").value = "";
+  document.getElementById("nome2").value = "";
+  document.getElementById("velocidade2").value = "";
+});
